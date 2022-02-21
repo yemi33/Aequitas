8bc463f52533397e0fb55d24bb626f3a2b275b89
Yemi Shin <shiny@carleton.edu>
2022-02-21 00:26:46 -0600

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,7 @@ from django.conf import settings
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
