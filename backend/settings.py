a7e6485fc5e093d00a9442999307f070b13fce71
Yemi Shin <shiny@carleton.edu>
2022-02-21 01:47:45 -0600

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -35,7 +35,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://example.com",
     "https://sub.example.com",
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://aequitasweb.herokuapp.com"
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
