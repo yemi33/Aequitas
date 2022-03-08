8f3aac40f9b01be890dda0be9d4bc034be7fd490
Yemi Shin <shiny@carleton.edu>
2022-03-07 22:29:19 -0600

    debugging web worker implementation

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 import os
 from django.conf import settings
 import django_heroku
-import urlparse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
